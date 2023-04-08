0eed39da67d53d9832d942f766f755dc28eb80d4
Tariq <rafiqtariq19@gmail.com>
2023-04-08 13:59:20 +0530

    features icons sizing fixed

--- src/components/features-desc.tsx
+++ src/components/features-desc.tsx
@@ -3,15 +3,15 @@ import '../styles/features.css'
 function Features() {
   return (
     <div className='feature-container'>
-        <h1 className='feature-content'>Gain more insight into how people use your</h1>
-        <p className='feature-content'>With our integrated CRM, project management, collaboration and invoicing capabilities, you can manage every aspect of your business in one secure platform.</p>
+        <h1 className='feature-content feature-heading'>Gain more insight into how people use your</h1>
+        <p className='grey-color feature-content'>With our integrated CRM, project management, collaboration and invoicing capabilities, you can manage every aspect of your business in one secure platform.</p>
         <div className='feature-cards'>
             {features.map((fdesc, index) => {
               return(
                 <div key={index} className="feature-card">
                   <div className='icon'><img src={fdesc.icon} alt="icon" /></div>
-                  <h3>{fdesc.title}</h3>
-                  <p style={{marginTop: '20px'}}>{fdesc.desc}</p>
+                  <h3 style={{margin: '20px'}}>{fdesc.title}</h3>
+                  <p className='grey-color' style={{marginTop: '20px'}}>{fdesc.desc}</p>
                 </div>
               )
             })}
